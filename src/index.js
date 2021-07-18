612c3355276dd5148b9dfe43efe6275fb651c93d
LapenokAlexey <aleks.lapenok@gmail.com>
2021-07-18 14:52:57 +0300

    fix: hashrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app';
-import { HashRouter } from 'react-router-dom';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
